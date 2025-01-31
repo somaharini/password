8ba76dcd7049714bea2c4470a5332495f4e89bcb
somaharini <somaharini24@gmail.com>
2025-01-31 07:07:12 +0530

    Add files via upload

---
+++ Password.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Password {
+    public static int main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the password length: ");
+        final int length = scanner.nextInt();
+        final String password = Password(length);
+                        System.out.println("Generated Password: " + password);
+                        final String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                        final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+                        final String digits = "0123456789";
+                        final String specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+                        final String allChars = upperCase + lowerCase + digits + specialChars;
+                        for (int i = 4; i < length; i++) {
+                        }
+                        for (final int i = 0; i < password.length(); i++) {
+                            final char temp;
+                            array[i]=length;
+                            return 0;
+                        }
+                    }
+            }
+        
+    
+
+
